0c0d5c3d03b560fc753a5e21d99eae40bbd27531
EySword <yche123@qq.com>
2021-04-21 23:00:20 +0800

    add

---
+++ script/test.py
@@ -0,0 +1,10 @@
+aim=1.15
+
+print(aim)
+percent_aim=((aim-1)*100)
+print(percent_aim)
+percent_aim=round(percent_aim/1)
+print(type(percent_aim))
+percent_aim=str(percent_aim)+'%'
+print(percent_aim)
+
