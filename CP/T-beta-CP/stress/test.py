924b479af552b4e29242123f520f23ca6b621d7d
EySword <yche123@qq.com>
2021-05-26 16:52:02 +0800

    add a lot

--- CP/T-beta-CP/stress/test.py
+++ CP/T-beta-CP/stress/test.py
@@ -1,12 +1,11 @@
-POSCAR0=open('POSCAR','r')
-'''
-指定a的梯度生成新POSCAR
-'''
 
-data=POSCAR0.readlines()
-# print(a)
-line_a=data[3]
-a=line_a[27:48]
-print(type(a))
-a=float(a)
-print(a)
+a=1.001
+
+
+print(a,type(a))
+b=round(((a-1)*100)/1,3)
+
+print(b,type(b))
+c=round(b)
+print(c,type(c))
+
